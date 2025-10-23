48266cd1c23c42a79d00ee628f8374d391420391
Kennedy Zinn <kennedyzinn@Kennedys-Laptop.local>
2025-10-23 17:18:14 -0500

    Added feature that prints number of poor quality probes

--- process_methyl_microarray.R
+++ process_methyl_microarray.R
@@ -2,8 +2,9 @@ process_methyl_microarray <- function(rgSet){
   
   # Detect p values
   p_values <- detectionP(rgSet, type = "m+u")
+  cat("The number of probes with at least one sample having a detection p-value above 0.01 is:", nrow(p_values[rowSums(p_values>0.01)>0,]))
   
-  mSet <- preprocessFunnorm(rgSet)
+  mSet <- preprocessFunnorm(rgSet, sex = rep(0, nrow(sample_sheet)))
   
   # ensure probes are in the same order in the mSetSq and detP objects
   p_values <- p_values[match(featureNames(mSet),rownames(p_values)),]
